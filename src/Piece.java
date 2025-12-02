4eec4806f14cafeb3973b969e6eaedbbeed20dac
Megatron8000 <barykin88@hotmail.com>
2025-12-02 18:05:11 +0400

    refactor Board and Piece, add methods isWhite - check color of figure and findKing mate checking

--- src/Piece.java
+++ src/Piece.java
@@ -1,16 +1,15 @@
-//абстрактный класс для всех фигур
-
 abstract class Piece {
-
-    boolean white;
+    protected boolean white;
 
     public Piece(boolean white) {
         this.white = white;
     }
 
+    public boolean isWhite() { return white; }
+
     abstract boolean canMove(Board board, Spot start, Spot end);
 
-    boolean sameColor(Piece a, Piece b) {
+    protected boolean sameColor(Piece a, Piece b) {
         return a != null && b != null && a.white == b.white;
     }
 }
